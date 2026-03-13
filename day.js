80ae22220c633359eb65997dc4ec781989889f26
deepika-1722 <langanoorunavadeepika@gmail.com>
2026-03-13 12:14:49 +0530

    Add files via upload

---
+++ day.js
@@ -0,0 +1,12 @@
+let day=4;
+if(day==1){
+    console.log("monday");
+} else if(day==2) {
+    console.log("tuesday");
+} else if(day==3) {
+    console.log("wednesday");
+} else if(day==4) {
+    console.log("thursday");
+} else {
+    console.log("no day");
+}
